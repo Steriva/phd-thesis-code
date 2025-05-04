05dfc71c47bc1a77d3e70f13831fa396341326d3
Steriva <stefano18.riva@mail.polimi.it>
2025-05-04 19:00:29 +0200

    New repo structure and notebooks for chap2 and 3 (up to sec 3.2.1)

---
+++ Notebooks/Chapter3/BoundarySensing/utils.py
@@ -0,0 +1,100 @@
+from matplotlib import patches
+from matplotlib import cm
+import numpy as np
+from scipy.interpolate import griddata
+
+from mpi4py import MPI
+import gmsh
+from dolfinx.io import gmshio
+
+def get_msfr_geometry(ax, show_ticks = False):
+    # Defining the rectangles for the blue zones
+    rect_blanket = patches.Rectangle((1.13, 0.186), 0.7, 1.884, linewidth=0.5, facecolor='white', edgecolor='black')
+
+    ax.add_patch(rect_blanket)
+
+    # Setting the limits and aspect
+    ax.set_xlim(0.01, 2.06450009)
+    ax.set_ylim(0, 2.25500011)
+    ax.set_aspect('equal')
+
+    if not show_ticks:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+def create_streamlines(domain, velocity):
+
+    x_grid = np.linspace(0, 2.05, 50)
+    y_grid = np.linspace(-2.26 / 2, 2.26 / 2, 50)
+    X, Y = np.meshgrid(x_grid, y_grid)
+
+    vel_2D = velocity.reshape(-1, 3)
+
+    U_interp = griddata(domain, vel_2D[:,0], (X, Y), method='linear')
+    V_interp = griddata(domain, vel_2D[:,2], (X, Y), method='linear')
+
+    return X, Y, U_interp, V_interp
+
+
+def plot_contour(ax, domain, _snap, 
+                 vec_mode_to_plot = None, 
+                 cmap = cm.jet, levels = 20,
+                 streamline_plot = False, density = 2, linewidth=0.75, show_ticks = False):
+    
+    if vec_mode_to_plot is not None:
+        if vec_mode_to_plot == 'x':
+            snap = _snap[:,::3]
+        elif vec_mode_to_plot == 'y':
+            snap = _snap[:,1::3]
+        elif vec_mode_to_plot == 'z':
+            snap = _snap[:,2::3]
+        else:
+            snap = np.linalg.norm(_snap.reshape(-1, domain.shape[1]), axis=1)
+    else:
+        snap = _snap
+
+    cont = ax.tricontourf(*domain.T, snap, cmap = cmap, levels = levels)
+
+    # Streamlines
+    if streamline_plot:
+        
+        strm = ax.streamplot(*create_streamlines(domain, _snap), 
+                            color='k', density=density, linewidth=linewidth, arrowstyle='->')
+
+    # Add Blanket
+    get_msfr_geometry(ax, show_ticks = show_ticks)
+
+    return cont
+
+def create_mesh_dolfinx(path):
+
+    mesh_comm = MPI.COMM_WORLD
+    model_rank = 0
+    gdim = 2
+     
+    # Initialize the gmsh module
+    gmsh.initialize()
+
+    # Load the .geo file
+    gmsh.merge(path)
+    gmsh.model.geo.synchronize()
+
+    # Set algorithm (adaptive = 1, Frontal-Delaunay = 6)
+    gmsh.option.setNumber("Mesh.Algorithm", 6)
+
+    # Linear Finite Element
+    gmsh.model.mesh.generate(gdim)
+    gmsh.model.mesh.optimize("Netgen")
+
+    # Import into dolfinx
+    domain, ct, ft = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim = gdim )
+
+    # Finalize the gmsh module
+    gmsh.finalize()
+        
+    ft.name = "Facet markers"
+
+    domain.topology.create_connectivity(gdim, gdim)
+    domain.topology.create_connectivity(gdim-1, gdim)
+    
+    return domain, ct, ft, gdim
